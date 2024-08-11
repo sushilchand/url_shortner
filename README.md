740c73307ef7463d38426f0a8a53d17457983710
Sushil Chand <sushilchand@Sushils-MacBook-Air.local>
2024-08-11 16:03:53 +0530

    feature: url shortner in DRF

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# url_shortner
+# Link Shortener Django
+
+## Table of Contents
+
+* [Getting Started](#getting-started)
+  + [Using Docker](#using-docker)
+
+## Getting Started
+
+### Using Docker
+
+- Clone the repo
+
+```
+git clone https://github.com/aerabi/link-shortener
+```
+
+- Bring up the app
+
+```
+docker-compose up -d --build
+```
+
+- Perform the migration
+
+```
+docker-compose exec web python manage.py migrate
+```
---
+++ src/urlshortener/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from . import service
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def redirect_hash(request, url_hash):
+    original_url = service.load_url(url_hash).original_url
+    return redirect(original_url)
+
+
+def shorten_post(request):
+    return shorten(request, request.POST['url'])
+
+
+def shorten(request, url):
+    shortened_url_hash = service.shorten(url)
+    shortened_url = request.build_absolute_uri(reverse('redirect', args=[shortened_url_hash]))
+    return render(request, 'main/link.html', {'shortened_url': shortened_url})
